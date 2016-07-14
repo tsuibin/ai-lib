50d0c21e5ed4e3a188506a8522553249f79326e0
tsuibin <63877278@qq.com>
2016-07-14 16:30:14 +0800

    update website

--- js/utils.js
+++ js/utils.js
@@ -1,9 +1,9 @@
-var webSite = "http://www.cazool.org/"
+var webSite = "http://www.o2ee.com/"
 $('#change_to_chinese').click( function(evt) {                                                                                                                        
         evt.stopPropagation();
         if ((pos = document.cookie.indexOf('LD_LANG=')) > 0 && document.cookie.substr(pos + 8, 5) == '%23cn');
         else
-            document.cookie = 'LD_LANG=%23cn;path=/;domain=.cazool.org;';
+            document.cookie = 'LD_LANG=%23cn;path=/;domain=.o2ee.com;';
             window.location.href = webSite + "index.cn.html";
         return;
     }   
@@ -13,7 +13,7 @@ $('#change_to_english').click( function(evt) {
         evt.stopPropagation();
         if ((pos = document.cookie.indexOf('LD_LANG=')) > 0 && document.cookie.substr(pos + 8, 5) == '%23en');
         else
-            document.cookie = 'LD_LANG=%23en;path=/;domain=.cazool.org;';
+            document.cookie = 'LD_LANG=%23en;path=/;domain=.o2ee.com;';
             window.location.href = webSite + "index.en.html";
         return;
     }   
