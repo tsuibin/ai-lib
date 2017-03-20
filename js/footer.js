6718195d6cd5e5a5602444ffbc0e3aeb481be54b
Tsuibin <tsuibin@o2ee.com>
2017-03-20 11:31:50 +0800

    update pages 1

--- js/footer.js
+++ js/footer.js
@@ -8,9 +8,6 @@ function showFooter()
         <ul class="side-nav"> \
             <li class="heading"><a href="#">社区</a></li> \
 			<li><a href="#">论坛</a></li> \
-			<li><a href="#">开发者</a></li> \
-            <li><a href="#">BUG管理</a></li> \
-            <li><a href="#">博客</a> \
 			</li></ul> \
         		</div> \
         		<div class="two column"> \
@@ -18,14 +15,12 @@ function showFooter()
             <li class="heading"><a href="#">关于我们</a></li> \
 			<li><a href="#">团队介绍</a></li> \
 			<li><a href="#">加入我们</a></li> \
-            <li><a href="#">网站地图</a></li> \
 			</ul> \
             	</div> \
             <div class="two columns"> \
             	<ul class="side-nav"> \
                     <li class="heading"><a href="#">联系我们</a></li> \
                     <li><a href="#">邮件列表</a></li> \
-                    <li><a href="#">友情链接</a></li> \
 				</ul> \
             </div> \
             <div class="two column"> \
@@ -41,13 +36,11 @@ function showFooter()
                 <a href="http://www.facebook.com/o2ee" class="social-icon facebook" rel="nofollow" onclick="_gaq.push([\'_trackEvent\', \'Interactions\', \'Footer\', \'Facebook\', 1, true]);"></a> \
                 <a href="https://twitter.com/o2ee" class="social-icon twitter" rel="nofollow" onclick="_gaq.push([\'_trackEvent\', \'Interactions\', \'Footer\', \'Twitter\', 1, true]);"></a> \
                 <a href="http://us.linkedin.com/company/o2ee" class="social-icon linkedin" rel="nofollow" onclick="_gaq.push([\'_trackEvent\', \'Interactions\', \'Footer\', \'LinkedIn\', 1, true]);"></a> \
-                <a href="https://plus.google.com/" class="social-icon google" rel="nofollow" onclick="_gaq.push([\'_trackEvent\', \'Interactions\', \'Footer\', \'Googleplus\', 1, true]);"></a> \
-                <a href="http://www.youtube.com/o2ee" class="social-icon youtube" rel="nofollow" onclick="_gaq.push([\'_trackEvent\', \'Interactions\', \'Footer\', \'Youtube\', 1, true]);"></a> \
         	</div> \
         	</div> \
         </div> \
             <div class="two column"> \
-                <p>中国·武汉 <br> 虎泉街291号 <br>© 2011-2013 O2EE Co.Ltd <br> 鄂ICP备14013274号</p> \
+                <p>中国·武汉 <br> 虎泉街291号 <br>© 2016-2017 O2EE Co.Ltd <br> 鄂ICP备14013274号</p> \
         	</div> \
         </div> \
    </div> \
--- js/menu.js
+++ js/menu.js
@@ -6,14 +6,14 @@ function navMenu()
                 <ul> \
                 <li><a href="#" id="change_to_chinese">中文</a></li> \
                 <li><a href="#" id="change_to_english_">ENGLISH</a></li> \
-                <li><a href="#">博客</a></li> \
+                <li><a href="#">论坛</a></li> \
                 </ul> \
             </div> \
         </div> \
 </section> \
 <nav id="main-nav" class="row"> \
-                <div id="logo" class="columns" style="width:33%"> \
-    	<a href=""><img src="img/logo_o2ee.png" alt="Formstack Online Form Builder"></a> \
+                <div id="logo" class="columns" style="width:25%"> \
+    	<a href=""><img src="img/logo.png" alt="Formstack Online Form Builder"></a> \
     </div> \
             <div id="site-links" class="columns" style="width:42%"> \
     	<ul class="nav-bar" id="mobile-submenu"> \
@@ -26,11 +26,8 @@ function navMenu()
             	<ul style="display: none;" class="flyout"> \
                 <li><a href="#">充电桩运营系统</a></li> \
                 <li><a href="#">Rapid开发套件</a></li> \
-                <li><a href="#">实时数据库</a></li> \
-                <li><a href="#">趋势分析工具</a></li> \
-                <li><a href="#">OPC Server</a></li> \
-                <li><a href="#">实时监控平台</a></li> \
-                <li><a href="#">组态工具</a></li> \
+                <li><a href="#">数据透传</a></li> \
+                <li><a href="#">O2mation</a></li> \
                      \
                 <!-- <li><a href="#">百科</a></li> \
                 <li><a href="#">BUG管理</a></li> --> \
@@ -45,10 +42,6 @@ function navMenu()
    			<li><a href="contact.cn.html">联系我们</a></li> \
 		</ul> \
     </div> \
-    <div id="trial-login" class="three columns"> \
-        <a href="#" class="button orange login">物联网</a> \
-        <a href="#" class="button green trial">新能源</a> \
- 	</div> \
 </nav>';
     document.getElementById("header").innerHTML=menu;
 }
